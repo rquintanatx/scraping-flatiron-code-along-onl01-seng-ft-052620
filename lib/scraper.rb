2f5e191ce905cc631ed5d4a6c7695f9e9d4dbeff
Robert Quintana <robertquintanatx@gmail.com>
2020-06-17 05:49:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
+  
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
