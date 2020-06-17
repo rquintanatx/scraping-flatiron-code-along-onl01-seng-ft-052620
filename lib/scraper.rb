1abb4024ba5db4923e45d8d008f1f14954187b41
Robert Quintana <robertquintanatx@gmail.com>
2020-06-17 05:39:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  def get_page
+    Nokogiri::HTML(open())
+  end 
   
   def print_courses
     self.make_courses
